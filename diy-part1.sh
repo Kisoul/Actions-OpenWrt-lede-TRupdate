56ccc3952914443ed2319504984490ab53f8a755
Kisoul <32985553+Kisoul@users.noreply.github.com>
2024-09-05 10:03:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,14 +16,14 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source （openwrt-packages is release version; small-package is beta version; small is passwall Library dependencies, golang is smpackage compilation tools）
+# Add a feed source （openwrt-packages is release version (small-package is beta version; small is passwall Library dependencies）
 #sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-# Insert the code into the first line to replace the ‘golang’ component in the source code
+# Insert the code into the first line to replace the ‘golang’ component in the source code (golang is smpackage compilation tools)
 sed -i '1i src-git golang https://github.com/kenzok8/golang feeds/packages/lang/golang.git' feeds.conf.default
 
 #Insert the code into the last line as a component supplement (prioritize using components from the source code)
