bab964fcb83814c7109410d78b0343e224a0499e
Kisoul <32985553+Kisoul@users.noreply.github.com>
2024-09-05 09:19:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,6 +28,3 @@ sed -i '1i src-git golang https://github.com/kenzok8/golang feeds/packages/lang/
 
 #Insert the code into the last line as a component supplement (prioritize using components from the source code)
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-
-# Add luci-app-xupnpd component in the source code
-sed -i '$a src-git xupnpd https://github.com/jarod360/luci-app-xupnpd.git' feeds.conf.default
