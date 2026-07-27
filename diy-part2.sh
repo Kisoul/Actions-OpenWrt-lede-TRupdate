4586b3a9f5e2f951555dd636e7f8b3f048ce4117
Kisoul <32985553+Kisoul@users.noreply.github.com>
2026-07-27 09:46:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -402,7 +402,7 @@ rm port_file
 # rm fix_file
 
 # Modify Openwrt's release
-sed -i 's/R24.8.1/R24.8.1 (Compiled by Kisoul on 09.16.2024)/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R26.05.20/R26.05.20 (Compiled by Kisoul on 07.27.2026)/g' package/lean/default-settings/files/zzz-default-settings
 
 # Write 'exit 0' at the end of the 'zzz-default-settings' file
 echo "exit 0" > fix_file
